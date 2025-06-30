0348731884e292af47a8f1bd0b19e8b194c60d0e
rayakinnera <kockie2023@gmail.com>
2025-06-30 23:05:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col, when_matched # Combined col and when_matched imports
 from snowflake.snowpark.context import get_active_session
 import pandas as pd
-from snowflake-snowpark-types
+from snowflake.snowpark.types import BooleanType
 streamlit.title("My Parents new Healthy Diner")
 st.write("Choose the fruits you want in your Custom Smoothie!") 
 
